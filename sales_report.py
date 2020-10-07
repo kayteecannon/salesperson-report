b7b01f260fd1eca94e58f9b259a0c59a64a651c3
vagrant <vagrant@vagrant.vm>
2020-10-07 01:47:49 +0000

    Adds docstrings, cleans up comments

--- sales_report.py
+++ sales_report.py
@@ -1,19 +1,9 @@
 """Generate sales report showing total melons each salesperson sold."""
 
-# Declare empty lists for salespeople and melons sold
-# TODO: Create a salespeople dictionary, include total order amount and melons sold
-
-
-# Open the sales report file
-# TODO: Create function to read files
-
 filename = 'sales-report.txt'
 
-
-# Iterate over each line in file
-# TODO: Create function to make dictionary
-
 def make_dictionary(filename):
+    '''Creates sales report dictionary from text file'''
 
     sales_dictionary = {}
 
@@ -30,36 +20,34 @@ def make_dictionary(filename):
         salesperson = entries[0]
         # Set melons equal to integer of third item in entries list
         melons = int(entries[2])
+        # Set order total equal to float of second item in entries list
         order_total = float(entries[1])
 
-        # If salesperson is found in salespeople list
-        # Set position equal to the index of salesperson
-        # Add melons to melons_sold list of salesperson
+        # If salesperson is found in sales dictionary
+        # Add melon count and order total to existing totals
         if salesperson in sales_dictionary.keys():
             sales_dictionary[salesperson]['melons sold'] = melons 
             sales_dictionary[salesperson]['order total'] = order_total
                     
-        # Otherwise, add salesperson to salespeople list
-        # And add melons to melons_sold list
+        # Otherwise, add new salesperson to sales dictionary
         else:
             sales_dictionary[salesperson] = {'melons sold': melons, 'order total': order_total}
             
     return sales_dictionary
             
 
-
-
-# For each salesperson, print salesperson and melons sold
-# TODO: Create function to print sales report
-
 def print_sales_report(filename):
+    '''Print sales report from text file'''
 
     sales_dictionary = make_dictionary(filename)
 
     for salesperson in sales_dictionary.keys():
+        # Capture salesperson information
         sales_values = sales_dictionary[salesperson]
         melons_sold = sales_values['melons sold']
         order_total = sales_values['order total']
+
         print(f'{salesperson} sold {melons_sold} melons for an order total of {order_total}')
+
 
 print_sales_report(filename)
